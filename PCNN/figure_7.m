2bfae4b3d8ebfd8cca4d5f563123c74b6847d93b
ritwikraha <rtzdzn@gmail.com>
2019-09-17 20:26:53 +0530

    demo image segmentation done

--- PCNN/figure_7.m
+++ PCNN/figure_7.m
@@ -8,11 +8,19 @@
 %   Archives of Computational Methods in Engineering, 2016.
 
 close all; clear,figure
-I = double(ones(256, 256))*230;
-I(65:192, 65:192) = 205;
+%I = double(ones(256, 256))*230;
+%I(65:192, 65:192) = 205;
+RGB = imread('a9.tif');
+% convert image to greyscale if its not b&w already
+if size(RGB,3) ~= 1
+I = rgb2gray(RGB);
+else
+I=RGB;    
+end
+I = double(I);
 S = I;
 
-S(:, 128:256) = S(:, 128:256)*0.5;
+%S(:, 128:256) = S(:, 128:256)*0.5;
 subplot(1,2,1), imshow(uint8(S))
 %____________________________________
 [r, c] = size(S);
@@ -20,15 +28,16 @@ Y = zeros(r,c); T = Y;
 W = fspecial('gaussian',7,1);
 F = S;
 beta = 2;
+alpha=0.001;
 Th = 255*ones(r,c);
-dT = 1;
+dT = exp(-alpha);
 Vt = 400;
 fire_num = 0;
 n = 0;
 while fire_num < r*c
     n = n + 1;
     L = imfilter(Y,W,'symmetric');
-    Th = Th - dT + Vt*Y;
+    Th = Th*exp(-alpha) + Vt*Y;
     fire = 1;
     while fire == 1
         Q = Y;
@@ -44,6 +53,6 @@ while fire_num < r*c
     fire_num = fire_num + sum(sum(Y));    
 end
 %____________________________________
-T = 256 - T;
+%T = 256 - T;
 subplot(1,2,2), imshow(uint8(T))
 % Io = S./T.*0.9; figure, imshow(uint8([I 255*Io]))
