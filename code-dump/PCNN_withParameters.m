7f25b82829bde3d8366abbdfea7462e941300e89
ritwikraha <rtzdzn@gmail.com>
2019-09-24 21:18:32 +0530

    rough planning done

---
+++ code-dump/PCNN_withParameters.m
@@ -0,0 +1,63 @@
+function R=PCNN_withParameters(matrix)
+% Calulate the firing times of neurons in PCNN
+% Please set the Para as follows:
+% Parameters for PCNN
+RGB = matrix;
+% convert image to greyscale if its not b&w already
+if size(RGB,3) ~= 1
+I = rgb2gray(RGB);
+else
+I=RGB;    
+end
+I = double(I);
+np=200;
+link_arrange=3;
+alpha_L=1;% 0.06931 Or 1
+alpha_Theta=0.2;
+beta=3;% 0.2 or 3
+vL=1.0;
+vTheta=20;
+
+%=============================================================
+% disp('PCNN is processing...')
+[p,q]=size(I);
+F_NA=abs(I);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialize the parameters.
+% You'd better change them according to your applications
+% alpha_L=1;
+% alpha_Theta=0.2;
+%  beta=3;
+% vL=1.00;
+% vTheta=20;
+% Generate the null matrix that could be used
+L=zeros(p,q);
+U=zeros(p,q);
+Y=zeros(p,q);
+Y0=zeros(p,q);
+Theta=zeros(p,q);
+% Compute the linking strength.
+center_x=round(link_arrange/2);
+center_y=round(link_arrange/2);
+W=zeros(link_arrange,link_arrange);
+for i=1:link_arrange
+    for j=1:link_arrange
+        if (i==center_x)&&(j==center_y)
+            W(i,j)=0;
+        else
+            W(i,j)=1./sqrt((i-center_x).^2+(j-center_y).^2);
+        end
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%beta, alpha_Theta, vTheta
+F=F_NA;
+for n=1:np
+    K=conv2(Y,W,'same');
+    L=K;
+    U=double(F).*(1+beta*L); % default U
+    Theta=exp(-alpha_Theta)*Theta+vTheta*Y;    
+    Y=im2double(U>Theta);
+    Y0=Y0+Y;
+end
+R=Y0;
--- code-dump/Rough.m
+++ code-dump/Rough.m
@@ -1,10 +1,12 @@
 %% importing the images
-I=imread('file_name');
+I=imread('a9.tif');
 I = im2double(I);
-I2=imread('file_name');
+I2=imread('b9.tif');
 I2 = im2double(I2);
+subplot(1,3,1), imshow(uint8(I))
+subplot(1,3,2), imshow(uint8(I2))
 %% initializing parameters
-nTimes=50;
+nTimes=5;
 del=0.0705;
 e=del/2;
 %M= Number of rows; N= Number of coloumns
@@ -14,6 +16,8 @@ for n=1:nTimes
     for i=1:M
         for j=1:N
             %perform pcnn and calculate YA and YB
+            YA=PCNN(I);
+            YB=PCNN(I2);
         end
     end
 end
@@ -30,5 +34,5 @@ for i=1:M
     end
 end
 
-imshow(F);
+subplot(1,3,3), imshow(uint8(F))
         
